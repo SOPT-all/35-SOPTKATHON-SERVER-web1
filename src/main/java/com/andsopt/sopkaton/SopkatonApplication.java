ef5f6710f7c960b74e03bc5e487b774490db2317
woals2840 <woals2840@gmail.com>
2024-11-21 20:16:29 +0900

    init-project

---
+++ src/main/java/com/andsopt/sopkaton/SopkatonApplication.java
@@ -0,0 +1,13 @@
+package com.andsopt.sopkaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopkatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopkatonApplication.class, args);
+	}
+
+}
