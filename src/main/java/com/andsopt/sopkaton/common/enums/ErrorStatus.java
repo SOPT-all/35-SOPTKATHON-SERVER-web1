f388c0b8474d5519023db6b7371cef639fe28526
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-11-23 15:40:23 +0900

    [FEAT] : CORS설정 및 템플릿 작성

--- src/main/java/com/andsopt/sopkaton/common/enums/ErrorStatus.java
+++ src/main/java/com/andsopt/sopkaton/common/enums/ErrorStatus.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorStatus {
-    NOT_FOUND_USER(HttpStatus.NOT_FOUND, " 아이디에 해당하는 유저를 찾을 수 없습니다"),
-    NOT_FOUND_TICKET(HttpStatus.NOT_FOUND, "아이디에 해당하는 티켓을 찾을 수 없습니다");
+
+    TEST(HttpStatus.NOT_FOUND, "test");
 
     private final HttpStatus status;
 
